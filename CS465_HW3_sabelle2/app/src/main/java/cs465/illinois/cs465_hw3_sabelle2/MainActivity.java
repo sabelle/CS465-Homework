8645fca11437ac7c7eb33121a26b94d618fc1bf3
Sabelle Huang <sabelle2@illinois.edu>
2020-11-12 13:13:44 -0800

    first commit

---
+++ CS465_HW3_sabelle2/app/src/main/java/cs465/illinois/cs465_hw3_sabelle2/MainActivity.java
@@ -0,0 +1,14 @@
+package cs465.illinois.cs465_hw3_sabelle2;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
